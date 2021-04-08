c5389ab3e650c92b8bd6f8f5b94efbdbe626a27d
Antoine-LB <antoine.stc25@gmail.com>
2021-04-08 16:37:57 +0200

    update doc test

--- src/startStrapi.js
+++ src/startStrapi.js
@@ -31,7 +31,6 @@ describe("Global setup", () => {
 
 const Strapi = require("strapi");
 const http = require("http");
-const fs = require("fs-extra");
 
 let instance;
 
@@ -52,31 +51,4 @@ async function startStrapi(callBack) {
   return instance;
 }
 
-async function startStrapiJest(callBack) {
-  /** this code is called once before any test is called */
-  beforeAll(async (done) => {
-    jest.setTimeout(50000);
-    await startStrapi(callBack); // singleton so it can be called many times
-    done();
-  });
-
-  /** this code is called once before all the tested are finished */
-  afterAll(async (done) => {
-    const dbSettings = strapi.config.get(
-      "database.connections.default.settings"
-    );
-
-    //delete test database after all tests
-    if (dbSettings && dbSettings.filename) {
-      // await putOriginalDB();
-      const tmpDbFile = `${process.env.PWD}/${dbSettings.filename}`;
-      if (fs.existsSync(tmpDbFile)) {
-        fs.unlinkSync(tmpDbFile);
-      }
-    }
-
-    done();
-  });
-}
-
-module.exports = { startStrapiJest };
+module.exports = { startStrapi };
